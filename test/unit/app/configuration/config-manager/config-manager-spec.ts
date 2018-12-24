65a0c003c75fe6ca28b1fce4f1ed83a729d7f065
Paul Drury <paul@drurys.org>
2018-12-24 12:05:17 +0000

    added unit test support

---
+++ test/unit/app/configuration/config-manager/config-manager-spec.ts
@@ -0,0 +1,10 @@
+import "mocha";
+import { expect } from "chai";
+
+describe("config-manager", () => {
+    describe("getConfig", () => {
+        it("should exist", () => {
+            expect(true).to.be.true;
+        });
+    });
+});
