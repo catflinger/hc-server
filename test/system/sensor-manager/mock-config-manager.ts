892d9b5f9c2fa7257e892c7331a1f796039adda4
Paul Drury <paul@drurys.org>
2018-12-31 14:56:22 +0000

    fixed typescript config problem

--- test/system/sensor-manager/mock-config-manager.ts
+++ test/system/sensor-manager/mock-config-manager.ts
@@ -1,5 +1,6 @@
-import { IConfigManager, IProgram, INamedConfig, IDatedConfig, ISensorConfig, IConfiguration } from "../../../src/types";
 import { injectable } from "inversify";
+
+import { IConfigManager, IProgram, IDatedConfig, IConfiguration } from "../../../src/types";
 
 @injectable()
 export class MockConfigManager implements IConfigManager {
